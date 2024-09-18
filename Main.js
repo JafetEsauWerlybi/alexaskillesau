48aa3fe720f3c8602af14cb3e8fe4a4beb8187e6
JafetEsauWerlybi <jeckooficial@gmail.com>
2024-09-18 10:42:38 -0600

    Agregado main

---
+++ Main.js
@@ -0,0 +1,32 @@
+// componente.js
+import React from 'react';
+
+const Componente = () => {
+  return (
+    <div>
+      <header>
+        <h1>Bienvenido a mi Componente</h1>
+      </header>
+      <main>
+        <section>
+          <h2>Sección de Introducción</h2>
+          <p>Este es un párrafo dentro de la sección de introducción.</p>
+          <button onClick={() => alert('¡Botón clickeado!')}>Haz clic en mí</button>
+        </section>
+        <section>
+          <h2>Otra Sección</h2>
+          <ul>
+            <li>Elemento de lista 1</li>
+            <li>Elemento de lista 2</li>
+            <li>Elemento de lista 3</li>
+          </ul>
+        </section>
+      </main>
+      <footer>
+        <p>&copy; 2024 Mi Componente</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Componente;
